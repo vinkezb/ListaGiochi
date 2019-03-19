2ec349f7cd72af2d04d94746cde9ad3fa3447e83
vinkezb <kevin.bra99@gmail.com>
2019-03-19 10:22:22 +0100

    component

--- ListaGiochi/src/app/app.module.ts
+++ ListaGiochi/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
+import { DetailGameComponent } from './detail-game/detail-game.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    EditGameComponent,
+    DetailGameComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
